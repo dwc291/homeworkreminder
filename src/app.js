7658310d29c6077f18f4b2b6fef463f44a0ad516
Dawson Chung <dwc291@nyu.edu>
2020-04-01 15:44:43 -0400

    added imaged for documentation

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
 //app.js
+const express = require('express');
+const session = require('express-session');
+const path = require('path');
+const app = express();
+require('./db');
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: false }));
