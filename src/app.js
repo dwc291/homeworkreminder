5877b0b8adc3491d5b7da7e694cdbf6300cc9a53
Dawson Chung <dwc291@nyu.edu>
2020-04-07 02:22:41 -0400

    progress on project

--- src/app.js
+++ src/app.js
@@ -6,7 +6,61 @@ const app = express();
 require('./db');
 const mongoose = require('mongoose');
 const passport = require('passport');
+const LocalStrategy = require('passport-local');
 
 app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname,'..', 'public')));
 app.use(express.urlencoded({ extended: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+const Class = mongoose.model('Class');
+
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+      User.findOne({ username: username }, function(err, user) {
+        if (err) { return done(err); }
+        if (!user) {
+          return done(null, false, { message: 'Incorrect username.' });
+        }
+        if (!user.validPassword(password)) {
+          return done(null, false, { message: 'Incorrect password.' });
+        }
+        return done(null, user);
+      });
+    }
+));
+
+app.get('/login', function(req, res){
+    res.render('login');
+});
+
+app.post('/login', passport.authenticate('local', { 
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true 
+}));
+
+app.post('/signup', function(req, res){
+
+});
+
+app.get('/classes/add', function(req, res){
+  res.render('class-add');
+});
+
+app.post('/classes/add', function(req, res){
+  new Class({
+    user: user,//to be implemented after authentication has been implemented
+    name: req.body.Class
+  }).save(function(err){
+    if(err){
+        console.log(err);
+    }
+    else{
+        res.redirect('/classes');
+    }
+  });
+});
+
+app.listen(3000);
--- src/db.js
+++ src/db.js
@@ -9,7 +9,7 @@ const mongoose = require('mongoose');
 const User = new mongoose.Schema({
   // username provided by authentication plugin
   // password hash provided by authentication plugin
-  classes:  [{ type: mongoose.Schema.Types.ObjectId, ref: 'className' }]
+  classes:  [{ type: mongoose.Schema.Types.ObjectId, ref: 'Class' }]
 });
 
 // homeworks within a class
@@ -33,9 +33,33 @@ const Exam = new mongoose.Schema({
 // a class list
 // * each list must have a related user
 // * a list can have 0 or more homeworks or exams
-const className = new mongoose.Schema({
+const Class = new mongoose.Schema({
   user: {type: mongoose.Schema.Types.ObjectId, ref:'User'},
   name: {type: String, required: true},
   homeworks: [Homework],
   exams: [Exam]
 });
+
+mongoose.model('Class', Class);
+
+// is the environment variable, NODE_ENV, set to PRODUCTION? 
+let dbconf;
+if (process.env.NODE_ENV === 'PRODUCTION') {
+// if we're in PRODUCTION mode, then read the configration from a file
+// use blocking file io to do this...
+    const fs = require('fs');
+    const path = require('path');
+    const fn = path.join(__dirname, '../config.json');
+    const data = fs.readFileSync(fn);
+
+// our configuration file will be in json, so parse it and set the
+// conenction string appropriately!
+    const conf = JSON.parse(data);
+    dbconf = conf.dbconf;
+} 
+else {
+// if we're not in PRODUCTION mode, then use
+    dbconf = 'mongodb://localhost/finalProject';
+}
+
+mongoose.connect(dbconf);
